55373a71d00094eaf77730f032802c6ff5541355
Şevval İclal Cebecioğlu <161520054+sevvaliclalcebecioglu@users.noreply.github.com>
2024-05-14 17:01:20 +0300

    .

--- src/app/components/list-group/list-group.component.ts
+++ src/app/components/list-group/list-group.component.ts
@@ -1,5 +1,12 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  EventEmitter,
+  Input,
+  OnInit,
+  Output,
+} from '@angular/core';
 
 // Genel bir input olan state için type oluşturuldu.
 export interface ListGroupItem {
@@ -10,15 +17,37 @@ export interface ListGroupItem {
 @Component({
   selector: 'app-list-group',
   standalone: true,
-  imports: [
-    CommonModule,
-  ],
+  imports: [CommonModule],
   templateUrl: './list-group.component.html',
   styleUrl: './list-group.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ListGroupComponent {
- @Input() items : ListGroupItem[] = [];
- // @Input: Bu component'in kullanıldığı yerden dışardan(kullanıldığı yerden) bir değer almasını sağlar.
- // State'in değerini dışarıdan(kullanıldığı yerden) da atanabilir hale getirir.
+export class ListGroupComponent implements OnInit {
+  @Input() initialSelectedItemId: string | null = null;
+  @Input() items: ListGroupItem[] = []; // <app-list-group [items]="categoryListGroupItems"></app-list-group>
+  // @Input: Bu component'in dışarıdan (kullanıldığı yerden) bir değer almasını sağlar.
+  // State'in değerini dışarıdan (kullanıldığı yerden) da atanabilir hale getirir.
+
+  @Output() changeSelect = new EventEmitter<{
+    selectedItemId: string | null;
+  }>(); // <app-list-group (changeSelect)="onListGroupItemClick($event)"></app-list-group>
+  // @Output: Bu component'ten dışarıya bir değer gönderilmesini sağlar.
+  // EventEmitter: Bu component'ten dışarıya bir değer gönderilmesini sağlar.
+  // Component'in kullanıldığı yerde bu event'i dinleyen bir metot olabilir.
+
+  selectedItemId: string | null = null;
+
+  ngOnInit(): void {
+    if (this.initialSelectedItemId)
+      this.selectedItemId = this.initialSelectedItemId;
+  }
+
+  onListGroupItemClick(item: ListGroupItem) {
+    this.selectedItemId = this.selectedItemId !== item.id ? item.id : null;
+    this.changeSelect.emit({ selectedItemId: this.selectedItemId });
+  }
+
+  isSelectedItem(itemId: string): boolean {
+    return this.selectedItemId === itemId;
+  }
 }
--- src/app/features/categories/components/category-list-group/categories/components/category-list-group/category-list-group/category-list-group.component.ts
+++ src/app/features/categories/components/category-list-group/categories/components/category-list-group/category-list-group/category-list-group.component.ts
@@ -1,7 +1,8 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, Output } from '@angular/core';
 import { CategoryListItem } from '../../../../models/category-list-item';
 import { ListGroupComponent, ListGroupItem } from '../../../../../../../../components/list-group/list-group.component';
+import { EventEmitter } from 'stream';
 
 
 
@@ -9,42 +10,43 @@ import { ListGroupComponent, ListGroupItem } from '../../../../../../../../compo
 @Component({
   selector: 'app-category-list-group',
   standalone: true,
-  imports: [
-    CommonModule,
-    ListGroupComponent
-  ],
+  imports: [CommonModule, ListGroupComponent],
   templateUrl: './category-list-group.component.html',
   styleUrl: './category-list-group.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CategoryListGroupComponent {
+  @Input() initialSelectedCategoryId: number | null = null;
+  @Output() changeSelect = new EventEmitter<{selectedCategory: CategoryListItem | null;}>();
+
   categoryList: CategoryListItem[] = [
-    { id: 1, name: "Dairy" },
-    { id: 2, name: "Fruits" },
-    { id: 3, name: "Vegetables" },
-    { id: 4, name: "Meat" },
-    { id: 5, name: "Fish" }
-  ];
-  // This is a nock data
-  // TODO: get data from backend
-
-  // Aşağıda her bir kategori için yeni bir obje oluşturuyoruz.
-
-  // map: array içindeki her bir elemanı dönüp yeni değerlerle, yeni bir array oluşturur.
+    { id: 1, name: 'Dairy' },
+    { id: 2, name: 'Fruits' },
+    { id: 3, name: 'Vegetables' },
+    { id: 4, name: 'Meat' },
+    { id: 5, name: 'Fish' },
+  ]; // This is a mock data //TODO: get data from backend
 
   get categoryListGroupItems(): ListGroupItem[] {
+    // map: array içindeki her bir elemanı dönüp yeni değerlerle, yeni bir array oluşturur.
     return this.categoryList.map((category) => {
-
-
-      const ListGroupItem: ListGroupItem = {
-       // ListGroupItem tipinde bir obje oluşturuldu.
+      // ListGroupItem tipinde bir obje oluşturuldu.
+      const listGroupItem: ListGroupItem = {
         id: category.id.toString(),
         label: category.name,
       };
+
       // Yeni bir array oluşturmak adına yeni değer geri döndürüldü.
-      return ListGroupItem;
+      return listGroupItem;
     });
   }
-  // Döndüğümüz objelerden sonra yeni bir array oluşmuş oldu.
 
+  onChangeSelect(event: { selectedItemId: string | null }) {
+    const selectedCategory: CategoryListItem | null = event.selectedItemId
+      ? this.categoryList.find(
+          (category) => category.id === Number.parseInt(event.selectedItemId!)
+        )!
+      : null;
+    this.changeSelect.emit({ selectedCategory });
+  }
 }
